de4c41292f6b37f33f2d6cb4223c51366cedb4d7
Zach Williams <ZachTheSloth@users.noreply.github.com>
2017-06-28 20:11:13 -0400

    Removed unnecessary starter code and formatted all .swift files.

--- BlackHorizon/GameViewController.swift
+++ BlackHorizon/GameViewController.swift
@@ -6,17 +6,24 @@
 //  Copyright © 2017 Zachary Williams. All rights reserved.
 //
 
+
+
+
 import UIKit
 import SpriteKit
 import GameplayKit
 
+
+
+
 class GameViewController: UIViewController {
+    
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content, including enditites and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
             
             // Get the SKScene from the loaded GKScene
@@ -31,35 +38,48 @@ class GameViewController: UIViewController {
                 
                 // Present the scene
                 if let view = self.view as! SKView? {
+                    
                     view.presentScene(sceneNode)
                     
                     view.ignoresSiblingOrder = true
                     
                     view.showsFPS = true
                     view.showsNodeCount = true
+                    
                 }
+                
             }
+            
         }
+        
     }
-
+    
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .portrait
         } else {
-            return .all
+            return .portrait
         }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    
+    
+    
 }
