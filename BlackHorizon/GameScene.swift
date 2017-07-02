ebf81bbd672fd2111781f665c8899d7aadc289f7
Zach Williams <ZachTheSloth@users.noreply.github.com>
2017-07-02 19:13:36 -0400

    Re-did the grid system (no longer using tileGrids, they suck). Includes new art & UI direction.

--- BlackHorizon/GameScene.swift
+++ BlackHorizon/GameScene.swift
@@ -10,20 +10,14 @@ class GameScene: SKScene {
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    
-    var landBackgroundTiles:SKTileMapNode! // Land background tile node.
+    var tileArray:[SKSpriteNode] = []
     
     
     override func didMove(to view: SKView) {
         
         // Called right before the scene is presented, used here to set up the scene's contents.
         
-        backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1) // Create black background.
+        initTiles()
         
     }
     
@@ -32,38 +26,21 @@ class GameScene: SKScene {
         
         // Called immidiately after the scene is loaded into view.
         
-        self.lastUpdateTime = 0
-        
-    }
-
-    
-    override func update(_ currentTime: TimeInterval) {
-        
-        // Called before each frame is rendered.
-        
-        // Initialize _lastUpdateTime if it has not already been initialized.
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update.
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities.
-        for entity in self.entities {
-            
-            entity.update(deltaTime: dt)
-            
-        }
-        
-        self.lastUpdateTime = currentTime
-        
     }
     
     
-    func setTileGroup(_ tileGroup: SKTileGroup, andTileDefinition tileDefinition: SKTileDefinition, forColumn column: Int, row: Int) {
+    // UTILITY FUNCTIONS
+    
+    
+    func initTiles() {
         
-        
+        var i = 0;
+        while(i < 81) {
+            var tile = SKSpriteNode()
+            tile = self.childNode(withName: "tile + \(i)") as! SKSpriteNode
+            self.tileArray.append(tile)
+            i += 1
+        }
         
     }
     
