7ba8c46fa5295c70d7abeb75e8e01ab0eea60db0
Zach Williams <ZachTheSloth@users.noreply.github.com>
2017-07-14 22:38:32 -0400

    Added button animations.

--- BlackHorizon/GameScene.swift
+++ BlackHorizon/GameScene.swift
@@ -7,6 +7,9 @@ import GameplayKit
 class GameScene: SKScene {
     
     
+    // GLOBAL VARIABLES
+    
+    
     var entities = [GKEntity]()
     var graphs = [String : GKGraph]()
     
@@ -17,6 +20,9 @@ class GameScene: SKScene {
     // Booleans that check which type of unit is going to be placed on a tile when you tap a tile.
     var builderButtonPressed = false;
     var fighterButtonPressed = false;
+    
+    
+    // FUNCTIONS
     
     
     override func didMove(to view: SKView) {
@@ -54,16 +60,19 @@ class GameScene: SKScene {
                     tiles[tileNum!].texture = SKTexture(imageNamed: "tile-building-blue")
                     builderButtonPressed = false
                     player1BuilderButton.texture = SKTexture(imageNamed: "button-builder-blue")
+                    animateButtonPress(buttonSprite: player1BuilderButton, isReversed: true)
                 }
                 // Player 1 builder button.
                 if name == "player1BuilderButton" {
                     if builderButtonPressed {
                         builderButtonPressed = false
                         player1BuilderButton.texture = SKTexture(imageNamed: "button-builder-blue")
+                        animateButtonPress(buttonSprite: player1BuilderButton, isReversed: true)
                     }
                     else {
                         builderButtonPressed = true
                         player1BuilderButton.texture = SKTexture(imageNamed: "button-builder-pressed")
+                        animateButtonPress(buttonSprite: player1BuilderButton, isReversed: false)
                     }
                 }
                 // Player 1 fighter button.
@@ -71,10 +80,12 @@ class GameScene: SKScene {
                     if fighterButtonPressed {
                         fighterButtonPressed = false
                         player1FighterButton.texture = SKTexture(imageNamed: "button-fighter-blue")
+                        animateButtonPress(buttonSprite: player1FighterButton, isReversed: true)
                     }
                     else {
                         fighterButtonPressed = true
                         player1FighterButton.texture = SKTexture(imageNamed: "button-fighter-pressed")
+                        animateButtonPress(buttonSprite: player1FighterButton, isReversed: false)
                     }
                 }
             }
@@ -107,6 +118,27 @@ class GameScene: SKScene {
         
         player1BuilderButton = self.childNode(withName: "player1BuilderButton") as! SKSpriteNode
         player1FighterButton = self.childNode(withName: "player1FighterButton") as! SKSpriteNode
+        
+    }
+    
+    
+    func animateButtonPress(buttonSprite: SKSpriteNode, isReversed: Bool) {
+        
+        let buttonScale = SKAction.scale(to: 0.9, duration: 0.1)
+        let buttonRotate = SKAction.rotate(byAngle: 0.5, duration: 0.1)
+        
+        let inverseButtonScale = SKAction.scale(to: 1.0, duration: 0.1)
+        let inverseButtonRotate = SKAction.rotate(byAngle: -0.5, duration: 0.1)
+        
+        let buttonPressAction = SKAction.group([buttonScale, buttonRotate])
+        let inverseButtonPressAction = SKAction.group([inverseButtonScale, inverseButtonRotate])
+        
+        if isReversed {
+            buttonSprite.run(inverseButtonPressAction)
+        }
+        else {
+            buttonSprite.run(buttonPressAction)
+        }
         
     }
     
